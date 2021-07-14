f124dc3ad81e1c7e0deffe1cb991ba1b1c016da0
Cinder <cinderrr@163.com>
2021-07-15 07:26:46 +0800

    add .gitignore

--- build.sh
+++ build.sh
@@ -9,5 +9,4 @@ BUILD_DIR=${SOURCE_DIR}/${BUILD_TYPE}
 mkdir -p $BUILD_DIR \
     && cd $BUILD_DIR \
     && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE $SOURCE_DIR \
-    && make $*\
-    && rm -rf $BUILD_DIR/
+    && make $*
